67abffa2223eb08a271730434978cf75ab78669a
iSuperstar <vquery.solrac@gmail.com>
2023-11-05 01:05:50 -0400

    ;'./

--- Project/problem3_equation.m
+++ Project/problem3_equation.m
@@ -3,3 +3,10 @@ function fhandle = problem3_equation(Re)
     fhandle = @(f) problem3_equation_internal(f, Re);
 end
 
+function residual = problem3_equation_internal(f, Re)
+    % This is the von Karman equation with Re as a parameter
+    lhs = 1 / sqrt(f);
+    rhs = 4 * log10(Re * sqrt(f)) - 0.4;
+    residual = lhs - rhs;
+end
+
